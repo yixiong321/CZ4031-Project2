6c3b2b51a4e2962d5f56e9ca206f2d141b9fb3f9
diamond-gold <diamond-gold@users.noreply.github.com>
2022-10-31 20:32:12 +0800

    Visualize json returned

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@
 #!/usr/bin/python
 import psycopg2
 import json
+from pathlib import Path
 
 def connect():
     """ Connect to the PostgreSQL database server """
@@ -16,7 +17,7 @@ def connect():
             host="localhost",
             database="TPC-H",
             user="postgres",
-            password="1234")
+            password="123")
         # create a cursor
         cur = conn.cursor()
         
@@ -26,40 +27,25 @@ def connect():
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
-        cur.execute("EXPLAIN (ANALYZE, VERBOSE, FORMAT JSON)" + '''select
-	ps_partkey,
-	sum(ps_supplycost * ps_availqty) as value
-from
-	partsupp,
-	supplier,
-	nation
-where
-	ps_suppkey = s_suppkey
-	and s_nationkey = n_nationkey
-	and n_name = 'GERMANY'
-	and ps_supplycost > 10000
-	and s_acctbal < 10000
-group by
-	ps_partkey having
-		sum(ps_supplycost * ps_availqty) > (
-			select
-				sum(ps_supplycost * ps_availqty) * 0.0001000000
-			from
-				partsupp,
-				supplier,
-				nation
-			where
-				ps_suppkey = s_suppkey
-				and s_nationkey = n_nationkey
-				and n_name = 'GERMANY'
-		)
-order by
-	value desc''')
+        print()
+        '''
+        uncomment and tab lines below except close() for testing all queries
+        may want to comment out print(x) for cleaner output
+        '''
+        '''
+        for i in range(1,11):
+            print('Query',i)
+            query = Path('Queries/q' + str(i) + '.sql').read_text()
+        '''
+        query = Path('Queries/q10.sql').read_text()
+        cur.execute("EXPLAIN (ANALYZE, VERBOSE, FORMAT JSON)" + query)
         rows = cur.fetchall()
         x = json.dumps(rows)
         print(x)
-       
-	# close the communication with the PostgreSQL
+        print()
+        for a in rows[0][0]:
+            display(a['Plan'])
+        # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -67,6 +53,118 @@ order by
         if conn is not None:
             conn.close()
             print('Database connection closed.')
+
+def display(plan, level=0):
+    print('    ' * level, end='')
+    if 'Join Type' in plan:# Loop/Join
+        print(plan['Join Type'] + ' ', end='')
+    print(plan['Node Type'] + ' ', end='')
+
+    explore(plan) # trying to find interesting keys
+
+    # args specific to type
+    # Sort
+    if 'Sort Method' in plan:
+        print('(' + plan['Sort Method'] + ')', end='')
+    # Seq scan
+    if 'Relation Name' in plan:
+        print('table:' + plan['Relation Name'] + ' ', end='')
+    # Aggregate
+    if 'Strategy' in plan:
+        print('(' + plan['Strategy'] + ') ', end='')
+    if 'Group Key' in plan:
+        print('BY ' + str(plan['Group Key']) + ' ', end='')
+    if 'Subplan Name' in plan:
+        print('AS ' + plan['Subplan Name'] + ' ', end='')
+    # Seq scan & aggregate
+    if 'Filter' in plan:
+        print('filter:' + plan['Filter']
+        .replace('::numeric','')
+        .replace('::bpchar','')
+        .replace('::date','')
+        + ' ', end='')
+    # Hash join
+    if 'Hash Cond' in plan:
+        print('cond:' + plan['Hash Cond'] + ' ', end='')
+    # Merge join
+    if 'Merge Cond' in plan:
+        print('cond:' + plan['Merge Cond'] + ' ', end='')
+    # Index only scan
+    if 'Index Cond' in plan:
+        print('cond:' + plan['Index Cond'] + ' ', end='')
+
+    if 'Alias' in plan:
+        print("AS " + plan['Alias'], end='')
+    print()
+    if 'Plans' in plan:
+        for p in plan['Plans']:
+            display(p,level + 1)
+
+def explore(plan):
+    # trying to find interesting keys
+    for key in plan.keys():
+        if key not in [
+        'Parent Relationship',
+        'Parallel Aware',
+        'Async Capable',
+        'Startup Cost',
+        'Total Cost',
+        'Plan Rows',
+        'Plan Width',
+        'Actual Startup Time',
+        'Actual Total Time',
+        'Actual Rows',
+        'Actual Loops',
+        'Output',# figure out what is this and is it useful
+        'Schema',
+        'Rows Removed by Filter',
+        'Workers',
+        'Workers Planned',
+        'Workers Launched',
+        'Single Copy',
+        'Sort Space Used',
+        'Sort Space Type',
+        'Partial Mode',
+        'Inner Unique',
+        'Sort Key', #useful?
+        'Index Name',
+        'Scan Direction',
+        'Rows Removed by Index Recheck',
+        'Cache Mode',
+        'Cache Key',
+        'Planned Partitions',
+        'HashAgg Batches',
+        'Peak Memory Usage',
+        'Disk Usage',
+        'Hash Buckets',
+        'Original Hash Buckets',
+        'Hash Batches',
+        'Original Hash Batches',
+        'Heap Fetches',
+        'Rows Removed by Join Filter',
+        'Join Filter', #useful?
+        'Full-sort Groups',
+        'Presorted Key',# Incremental Sort, useful?
+        'Cache Hits',
+        'Cache Misses',
+        'Cache Evictions',
+        'Cache Overflows',
+        #used
+        'Node Type',
+        'Join Type',
+        'Sort Method',
+        'Relation Name',
+        'Strategy',
+        'Group Key',
+        'Subplan Name',
+        'Filter',
+        'Hash Cond',
+        'Merge Cond',
+        'Index Cond',
+        'Alias',
+        'Plans'
+        ]:
+            print('***Interesting?:"'+ key + '" value:' + str(plan[key]), end=',')
 
 
 connect()
