5e77c5e5df5dbdf69109f0758f7f69144c94b8fd
siti-nur-ummaira <sitiphang@gmail.com>
2022-11-13 23:47:05 +0800

    nodetypes->preprocessing

--- USETHISFOLDER/preprocessing.py
+++ USETHISFOLDER/preprocessing.py
@@ -1,10 +1,31 @@
 # !/usr/bin/python
 import psycopg2
 import json
-from nodetypes import node_types_dict
 import sqlparse
 import re
 
+node_types_dict={
+    'Aggregate': 'ENABLE_HASHAGG',
+    'Gather Merge': 'ENABLE_GATHERMERGE',
+    'Sort': 'ENABLE_SORT',
+    'Seq Scan': 'ENABLE_SEQSCAN',
+    #'Limit': 'Limit',
+    'Nested Loop': 'ENABLE_NESTLOOP',
+    'Index Scan': 'ENABLE_INDEXSCAN',
+    'Hash Join': 'ENABLE_HASHJOIN',
+    #'Hash': 'Hash',
+    'Merge Join': 'ENABLE_MERGEJOIN',
+    'Incremental Sort': 'ENABLE_INCREMENTAL_SORT',
+    'Memoize': 'ENABLE_MEMOIZE',
+    'Materialize': 'ENABLE_MATERIAL',
+    #'Gather': 'Gather'
+     }
+
+# note that setting this will not totally remove it,but only push it as a the last resort.
+AQP_configs=['ENABLE_BITMAPSCAN','ENABLE_GATHERMERGE','ENABLE_HASHAGG','ENABLE_HASHJOIN',
+'ENABLE_INCREMENTAL_SORT','ENABLE_INDEXSCAN','ENABLE_INDEXONLYSCAN','ENABLE_MERGEJOIN',
+'ENABLE_NESTLOOP','ENABLE_MATERIAL','ENABLE_MEMOIZE',
+'ENABLE_PARALLEL_HASH','ENABLE_SEQSCAN','ENABLE_SORT']
 
 def get_unique_node_types_dic(level, dic):
     if level["Node Type"] not in dic:
